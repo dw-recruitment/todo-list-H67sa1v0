30335fae881e5366aa1d78a46c67a7b551550021
Anonymous Candidate <anon@example.com>
2016-03-18 22:48:22 -0400

    New Duct project with site middleware
    https://github.com/weavejester/duct

---
+++ src/todo/system.clj
@@ -0,0 +1,29 @@
+(ns todo.system
+  (:require [clojure.java.io :as io]
+            [com.stuartsierra.component :as component]
+            [duct.component.endpoint :refer [endpoint-component]]
+            [duct.component.handler :refer [handler-component]]
+            [duct.middleware.not-found :refer [wrap-not-found]]
+            [duct.middleware.route-aliases :refer [wrap-route-aliases]]
+            [meta-merge.core :refer [meta-merge]]
+            [ring.component.jetty :refer [jetty-server]]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.webjars :refer [wrap-webjars]]))
+
+(def base-config
+  {:app {:middleware [[wrap-not-found :not-found]
+                      [wrap-webjars]
+                      [wrap-defaults :defaults]
+                      [wrap-route-aliases :aliases]]
+         :not-found  (io/resource "todo/errors/404.html")
+         :defaults   (meta-merge site-defaults {:static {:resources "todo/public"}})
+         :aliases    {"/" "/index.html"}}})
+
+(defn new-system [config]
+  (let [config (meta-merge base-config config)]
+    (-> (component/system-map
+         :app  (handler-component (:app config))
+         :http (jetty-server (:http config)))
+        (component/system-using
+         {:http [:app]
+          :app  []}))))

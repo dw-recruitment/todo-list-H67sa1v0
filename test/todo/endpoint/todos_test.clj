d4eaf96342baa4bfb0936cb70bc43d792dca133f
Anonymous Candidate <anon@example.com>
2016-03-21 13:08:07 -0400

    Adds a test

--- test/todo/endpoint/todos_test.clj
+++ test/todo/endpoint/todos_test.clj
@@ -3,7 +3,9 @@
             [todo.endpoint.todos :as todos]
             [todo.test-utils :as utils :refer [conn]]
             [todo.data.todos :as data]
-            [ring.mock.request :as mock]))
+            [ring.mock.request :as mock]
+            [ring.util.http-predicates :refer [see-other? ok? success?]]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (use-fixtures :each
   utils/wrap-manage-datomic)
@@ -15,17 +17,28 @@
     (doseq [[text status] todos]
       (data/create conn text status))))
 
-(deftest root-path-works
+(deftest root-route-works-for-get
   (let [handler (todos/todos-endpoint {:db {:conn conn}})]
     (testing "empty table"
       (let [resp (handler (mock/request :get "/"))]
-        (is (= 200 (:status resp)))
+        (is (ok? resp))
         (is (re-find #"<th>Todo</th>" (:body resp)))
         (is (re-find #"<th>Status</th>" (:body resp)))))
     (testing "with some data"
       (test-data)
       (let [resp (handler (mock/request :get "/"))]
-        (is (= 200 (:status resp)))
+        (is (ok? resp))
         (is (re-find #"Todo number 1" (:body resp)))
         (is (re-find #"Todo number 2" (:body resp)))
         (is (re-find #"Todo number 3" (:body resp)))))))
+
+(deftest root-route-works-for-post
+  (let [handler (wrap-defaults (todos/todos-endpoint {:db {:conn conn}})
+                               (assoc site-defaults :security false))]
+    (testing "the happy path"
+      (let [resp (handler (-> (mock/request :post "/")
+                              (mock/body {"todo-text" "A new todo"})))]
+        (is (see-other? resp))
+        (let [resp (handler (mock/request :get (get-in resp [:headers "Location"])))]
+          (is (ok? resp))
+          (is (re-find #"A new todo" (:body resp))))))))

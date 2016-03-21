3f1e15525d0bd97ed587ad2955760cb2504fdee9
Anonymous Candidate <anon@example.com>
2016-03-21 12:46:04 -0400

    Adds a form for submitting new todo items

--- src/todo/endpoint/todos.clj
+++ src/todo/endpoint/todos.clj
@@ -2,15 +2,26 @@
   (:require [compojure.core :refer :all]
             [todo.data.todos :as todos-data]
             [todo.endpoint.views.layout :as layout]
-            [todo.endpoint.views.todos :as todos-views]))
+            [todo.endpoint.views.todos :as todos-views]
+            [ring.util.http-response :refer :all]))
 
 (defn render-todos
   [conn]
   (-> (todos-data/index conn)
       (todos-views/todo-table)
-      (layout/wrap-layout)))
+      (layout/wrap-layout)
+      (ok)
+      (content-type "text/html")))
+
+(defn add-todo [conn text]
+  (try
+    (todos-data/create conn text)
+    (see-other "/")
+    (catch Throwable t
+      (internal-server-error! (.getMessage t)))))
 
 (defn todos-endpoint [config]
   (let [conn (-> config :db :conn)]
     (routes
-     (GET "/" [] (render-todos conn)))))
+     (GET "/" [] (render-todos conn))
+     (POST "/" [todo-text] (add-todo conn todo-text)))))

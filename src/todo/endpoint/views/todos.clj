32a6a6c186dd53ff2bd07da92371788c38f6d3a1
Anonymous Candidate <anon@example.com>
2016-03-22 23:41:02 -0400

    Expose UI elements for delete and edit

--- src/todo/endpoint/views/todos.clj
+++ src/todo/endpoint/views/todos.clj
@@ -31,36 +31,59 @@
              :placeholder "Enter new todo item"}]]
    (anti-forgery-field)])
 
+(defn page-header
+  [t-list]
+  [:div
+   [:div {:class "row"}
+    [:div {:class "col-md-8"}
+     [:div {:class "page-header"}
+      [:h3 (todo-lists/title t-list)]
+      [:form {:method "POST" :action (utils/todo-list-path t-list)}
+       [:a {:class "btn btn-default btn-xs"
+            :role "button"
+            :href (utils/edit-list-path t-list)}
+        "Edit"]
+       [:span "&nbsp;"]
+       [:button {:type "submit"
+                 :class "btn btn-xs btn-danger"
+                 :name "delete"
+                 :value "true"}
+        "Delete"]
+       (anti-forgery-field)]]]]])
+
 (defn todo-table
   "List the todos"
   [t-list entities]
-  [:div {:class "row"}
-   [:div {:class "col-md-8"}
-    [:h3 (todo-lists/title t-list)]
-    [:table {:class "table-striped table"}
-     [:thead
-      [:tr
-       [:th "Todo"]
-       [:th "Status"]
-       [:th "Actions"]]]
-     [:tbody
-      (for [e entities]
-        [:form {:method "POST" :action (utils/todo-path t-list e)}
-         [:input {:type "hidden"
-                  :name "todo-status"
-                  :id "todo-status"
-                  :value (update-status e)}]
-         (anti-forgery-field)
-         [:tr
-          [:td (maybe-wrap-strikethrough e)]
-          [:td (status e)]
-          [:td
-           [:button {:type "submit"
-                     :class "btn btn-success col-md-5"}
-            (button-label e)]
-           [:button {:type "submit"
-                     :name "delete"
-                     :value "true"
-                     :class "btn btn-danger pull-right col-md-5"}
-            "Delete"]]]])]]
-    (new-todo-form t-list)]])
+  [:div
+   (page-header t-list)
+   [:div {:class "row"}
+    [:div {:class "col-md-8"}
+     [:table {:class "table-striped table"}
+      [:thead
+       [:tr
+        [:th "Todo"]
+        [:th "Status"]
+        [:th "Actions"]]]
+      [:tbody
+       (for [e entities]
+         [:form {:method "POST" :action (utils/todo-path t-list e)}
+          [:input {:type "hidden"
+                   :name "todo-status"
+                   :id "todo-status"
+                   :value (update-status e)}]
+          (anti-forgery-field)
+          [:tr
+           [:td (maybe-wrap-strikethrough e)]
+           [:td (status e)]
+           [:td
+            [:button {:type "submit"
+                      :class "btn btn-success col-md-5 btn-sm"}
+             (button-label e)]
+            [:button {:type "submit"
+                      :name "delete"
+                      :value "true"
+                      :class "btn btn-danger pull-right col-md-5 btn-sm"}
+             "Delete"]]]])]]]]
+   [:div {:class "row"}
+    [:div {:class "col-md-8"}
+     (new-todo-form t-list)]]])
